979519c7c01fa60f78b3e0459a6e262736a4e6b9
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-07-24 00:47:46 +0900

    initial commit

---
+++ TestSDKMediation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestSDKMediation
+//
+//  Created by 清 貴幸 on 2017/07/24.
+//  Copyright © 2017年 daybysay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
