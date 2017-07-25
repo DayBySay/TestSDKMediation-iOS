f3d4c3b9f58fd25a756a82373d2049bb36ac16c7
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-07-25 20:01:13 +0900

    adapterのアレを追加

---
+++ Adapters/VideoRewardMaioAdapter.h
@@ -0,0 +1,13 @@
+//
+//  VideoRewardMaioAdapter.h
+//  TestSDKMediation
+//
+//  Created by 清 貴幸 on 2017/07/24.
+//  Copyright © 2017年 daybysay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VideoRewardMaioAdapter : NSObject
+
+@end
---
+++ Adapters/VideoRewardMaioAdapter.m
@@ -0,0 +1,19 @@
+//
+//  VideoRewardMaioAdapter.m
+//  TestSDKMediation
+//
+//  Created by 清 貴幸 on 2017/07/24.
+//  Copyright © 2017年 daybysay. All rights reserved.
+//
+
+#import "VideoRewardMaioAdapter.h"
+@import Maio;
+
+@interface VideoRewardMaioAdapter() <MaioDelegate>
+@end
+
+@implementation VideoRewardMaioAdapter
+
+
+
+@end
